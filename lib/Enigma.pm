d45fdfe6abb725ad0a7a8a64ee6abc4fd2a9ede7
pfwaggs <pfwaggs@townm.org>
2015-11-29 20:24:02 -0500

    simple rename of enigma3.pl so we can view this as a simple install Enigma.pl now has a menu selector.

--- lib/Enigma.pm
+++ lib/Enigma.pm
@@ -478,4 +478,49 @@ sub Encrypt_interactive {
 }
 #ZZZ
 
+# Menu_pick #AAA
+sub Menu_pick {
+    # input options :
+    #	clear screen: (1)/0
+    #	max: -1/(1)/n/n+
+    #	header: undef
+    #	prompt: pick lines:
+    #	preset: undef
+    #
+    # input menu :
+    # input array < \@ or @
+
+# defaults #AAA
+    my %opts = (clear=>1, max=>1, header=>undef, prompt=>'pick lines: ',preset=>undef,);
+    %opts = (%opts, %{shift @_}) if ref $_[0] eq 'HASH';
+    my @data = ref $_[0] eq 'ARRAY' ? @{shift @_} : @_;
+    my $max = $opts{max} == -1 ? @data : $opts{max};
+#ZZZ
+
+    my $picked = '*';
+    my $select = $picked^' ';
+    my @choices = (' ') x @data;
+
+    my @_menu = map {{str=>$data[$_], s=>' ', x=>1+$_}} keys @data;
+    if (defined $opts{preset}) {
+	$_menu[$opts{preset}]{s} ^= $select;
+    }
+    my $picks;
+    while (1) {
+	system('clear') if $opts{clear};
+	say $opts{header} if defined $opts{header};
+	say join(' : ',@{$_}{qw{s x str}}) for @_menu;
+	print $opts{prompt};
+	chomp ($picks = <STDIN>);
+	last if $picks =~ /^(?i)q/;
+	map {$_menu[$_-1]{s}^=$select} $picks =~ /^(?i)a/ ? (1..$max) : split(/\D/,$picks);
+    } continue {
+	last if ($max == grep {$_->{s} eq $picked} @_menu) and ($picks !~ /^(?i)a/);
+    }
+    my @found = map {$_->{x}-1} grep {$_->{s} eq $picked} @_menu;
+    my @rtn = @found <= $max ? @found : @found[0..$max-1];
+    return wantarray ? @rtn : \@rtn;
+}
+#ZZZ
+
 1;
